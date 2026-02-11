6aacd48eb45f99eb80742e1a6fd10339be5498c3
Lamabinslmah <lamarch5@hotmail.com>
2026-02-11 23:20:06 +0300

    complete but needs a little fix

---
+++ Model/StoreCategory.swift
@@ -0,0 +1,31 @@
+//
+//  StoreCategory.swift
+//  Shiddaha
+//
+//  Created by AlAnoud Alsaaid on 23/08/1447 AH.
+//
+
+import Foundation
+
+// MARK: - STORE CATEGORY
+enum StoreCategory: String, Identifiable, CaseIterable {
+    case tents
+    case characters
+    
+    var id: String { rawValue }
+    
+    var iconAssetName: String {
+        switch self {
+        case .tents: return "button_tent"
+        case .characters: return "ch_button"
+        }
+    }
+}
+
+// MARK: - STORE ITEM
+struct StoreItem: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let price: Int
+    let category: StoreCategory
+}
